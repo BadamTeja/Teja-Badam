74e3eb3dc68f935137ef738d3ff063abebadf484
BadamTeja <146067616+BadamTeja@users.noreply.github.com>
2023-09-27 17:52:24 +0530

    Add files via upload

---
+++ Task6/Task6.js
@@ -0,0 +1,27 @@
+function varExample(){
+    if(true){
+        var a="E-school";
+        console.log(a);
+    }
+    a="It is the best learning Institution";
+    console.log(a);
+    
+}
+varExample();
+
+function letExample(){
+    if(true){
+        let b="Benifits";
+        console.log(b);
+    }
+    b="Programs, Certifications and Affordability";
+    console.log(b);
+}
+letExample();
+
+function constExample(){
+    const c="E-school Educational Institutions";
+    console.log(c);
+    
+}
+constExample();
